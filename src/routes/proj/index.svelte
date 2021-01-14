65e7f91b4478cbefdad8c4b652a25ae7593b5834
Nathan Hare <me@laspruca.nz>
2021-01-14 17:05:59 +1300

    Complete all projects page

--- src/routes/proj/index.svelte
+++ src/routes/proj/index.svelte
@@ -1,31 +1,34 @@
 <script lang="ts">
   import Header from "../../components/Header.svelte";
-    export let projects: any[];
+  export let projects: any[];
 </script>
 
 <script lang="ts" context="module">
-    export async function preload() {
-        const request = await this.fetch("/proj/all-projects.json");
-        const projects = await request.json();
+  export async function preload() {
+    const request = await this.fetch("/proj/all-projects.json");
+    const projects = await request.json();
 
-        return {
-          projects
-        };
-    }
+    return {
+      projects
+    };
+  }
 </script>
 
 <style lang="scss">
-    @import "../../node_modules/assets/style/routes/proj/index.scss";
+  @import "../../node_modules/assets/style/routes/proj/index.scss";
 </style>
 
-<Header />
+<Header/>
 
 <div class="content">
     <div class="projects">
-        <div class="project">
-            {#each projects as project}
-                <h1>{project.metadata.title}</h1>
-            {/each}
-        </div>
+        {#each projects as project}
+            <a href="/proj/{project.slug}" class="project-wrapper">
+                <div class="project">
+                    <h1>{project.metadata.title}</h1>
+                    <h2>{project.metadata.description}</h2>
+                </div>
+            </a>
+        {/each}
     </div>
 </div>
