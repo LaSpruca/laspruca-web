25b8ba30a735a7833417b88c6ecc603195eaa309
Nathan Hare <me@laspruca.nz>
2021-07-14 21:27:18 +1200

    More header epicness

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import logo from './spruce-logo.svg';
+	import { page } from '$app/stores';
 </script>
 
 <header>
@@ -10,16 +11,27 @@
 		</a>
 
 		<nav>
-			<a href="/contact"><h2>Contact</h2></a>
-			<a href="/"><h2>Home</h2></a>
-			<a href="/projects"><h2>Projects</h2></a>
+			<a href="/contact" class={$page.path == '/contact' ? 'selected' : ''}
+				><h2>Contact</h2>
+				<div class="underline" />
+			</a>
+
+			<a href="/" class={$page.path == '/' ? 'selected' : ''}
+				><h2>Home</h2>
+				<div class="underline" />
+			</a>
+
+			<a href="/projects" class={$page.path.startsWith('/projects') ? 'selected' : ''}
+				><h2>Projects</h2>
+				<div class="underline" />
+			</a>
 		</nav>
 	</div>
 </header>
 
 <style lang="scss">
 	header {
-		background-image: url('/assets/img/header-bg.png');
+		background-image: url('/assets/img/header-bg.webp');
 		background-size: cover;
 		background-position: 0 50%;
 	}
@@ -28,7 +40,7 @@
 		padding: 5em;
 		width: 100%;
 		height: 100%;
-		backdrop-filter: blur(5px);
+		backdrop-filter: blur(20px);
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -68,25 +80,39 @@
 		width: 50%;
 
 		a {
-			color: white;
+			outline: none;
+			color: grey;
 			text-decoration: none;
-
 			text-align: center;
-		}
-	}
+			padding-bottom: 5px;
+			display: inline-block;
 
-	@media only screen and (max-width: 770px) {
-		.header-blur {
-			padding: 3em;
-		}
+			& .underline {
+				height: 3px;
+				background-color: transparent;
+				width: 0%;
+				transition: width 0.2s, background-color 0.5s, height 0.3s, margin 0.3s;
+				margin: 0 auto;
+				height: 3px;
+				margin-bottom: 3px;
+			}
 
-		nav {
-			margin-top: 0.5rem;
-			flex-direction: column;
-			align-items: center;
-			a {
-				width: auto;
-				margin: 0.25em 0;
+			&:hover {
+				color: lightgray;
+				.underline {
+					background-color: lightgrey;
+					width: 100%;
+				}
+			}
+
+			&.selected {
+				color: white;
+				.underline {
+					width: 100%;
+					background-color: white;
+					height: 6px;
+					margin-bottom: 0px;
+				}
 			}
 		}
 	}
