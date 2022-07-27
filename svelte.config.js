94c742ac24dad08e2d7a2850a880463c7033c0d2
Nathan Hare <me@laspruca.nz>
2022-07-27 23:00:14 +1200

    Add static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import { argv } from 'process';
+
+const staticFlag = argv.some((x) => x === '--static');
+const adapter = staticFlag ? staticAdapter : autoAdapter;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,10 +16,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [preprocess({}), mdsvex(mdsvexConfig)],
 
-	extensions: ['.svelte', '.md'],
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: { default: true }
 	}
 };
 
