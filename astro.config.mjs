a2882a7ccd9a4751d54c680e5f4a5f7a5ea4d4d6
Nathan Hare <me@laspruca.nz>
2022-11-04 23:09:44 +1300

    Fix broken project order

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    syntaxHighlight: "prism",
-  },
   output: "static",
   site: "https://laspruca.nz",
-  integrations: [sitemap(), image({})],
+  integrations: [sitemap(), image()],
 });
