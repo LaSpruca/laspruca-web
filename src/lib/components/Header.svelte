fa0efc5d2b34d30a1700706162f46de89194cdcc
Connor  Hare <me@laspruca.nz>
2024-09-23 14:00:18 +1200

    fix: header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,8 +2,10 @@
 	import { page } from '$app/stores';
 </script>
 
+<div class="fixed z-40 h-[calc(1.25rem+120px)] w-full backdrop-blur-md md:h-[4.75rem]"></div>
+
 <div
-	class="fixed left-5 top-5 flex w-[calc(100vw-2.5rem)] flex-col items-center justify-between gap-5 rounded-lg bg-glass p-5 shadow-sm shadow-shadow backdrop-blur-[15rem] md:h-14 md:flex-row"
+	class="fixed left-5 top-5 z-50 flex w-[calc(100vw-2.5rem)] flex-col items-center justify-between gap-5 rounded-lg bg-glass p-5 shadow-sm shadow-shadow backdrop-blur-[15rem] md:h-14 md:flex-row"
 >
 	<a class="text-xl font-bold text-primary" href="/">Connor Hare</a>
 
